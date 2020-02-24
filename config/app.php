0a4a3c7be3326380ff959b57a35a317f8f6d1276
Leo Yang <muyi599@qq.com>
2020-02-24 11:34:35 +0800

    一个不能安装的composer包

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Cxp\Avatar\AvatarProvider::class,
+        
     ],
 
     /*
@@ -226,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Avatar' => Cxp\Avatar\Facades\Avatar::class,
     ],
 
 ];
