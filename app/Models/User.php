61219d290f7eea3b09cda5635df87f89aa5d5d8c
Leo Yang <muyi599@qq.com>
2020-02-29 17:05:08 +0800

    用户信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,7 @@ class User extends Authenticatable implements MustVerifyEmail,JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'weixin_openid', 'weixin_unionid'
     ];
 
     /**
